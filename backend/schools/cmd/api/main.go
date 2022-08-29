dafeb4166310d32eddfcce0591b8d1e03bce3029
michael <2018118090@ub.edu.bz>
2022-08-29 13:20:23 -0600

    default simple webserver

--- backend/schools/cmd/api/main.go
+++ backend/schools/cmd/api/main.go
@@ -3,9 +3,61 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
 )
 
+// The application version number
+const version = "1.0.0"
+
+// The configuration settings
+type config struct {
+	port int
+	env  string //development, staging, producation, etc
+}
+
+// Dependency Injection
+type application struct {
+	config config
+	logger *log.Logger
+}
+
+// main
 func main() {
-	fmt.Println("Hello World! - by louie zong")
+	var cfg config
+	//read in the flags that are needed to populate our config
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")                                            //values being used in struct therefore IntVar will be used
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development | staging | production)") //just like IntVar; StringVar is also being used
+	flag.Parse()
+
+	//create a logger
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime) //no prefix for now
+
+	//create an instance of our application struct
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	//create our server mux
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
+
+	//create out HTTP server
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      mux,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	//Start our server
+	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
+	err := srv.ListenAndServe()
+	logger.Fatal(err)
 }
